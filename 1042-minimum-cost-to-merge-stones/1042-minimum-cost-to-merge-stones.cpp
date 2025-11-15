29a7074190cee5694ed06e56c89bbbe3e4463773
TanishkChauhan09 <iamtanishkchauhan@gmail.com>
2025-11-15 18:30:14 +0530

    Time: 0 ms (100%), Space: 11.3 MB (43.76%) - LeetHub

--- 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.cpp
+++ 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.cpp
@@ -1,39 +1,11 @@
 class Solution {
 public:
-
     
-    int find(int i,int j,int k,vector<int>&prefix_sum,vector<vector<int>>&dp)
-    {
-        // Base case
-        if(i>=j) return 0;
-
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        
-        int ans = INT_MAX;
-        for(int partition=i;partition<j;partition+=(k-1))
-        {
-            // left part is returning the min sum of 'k' consecutive numbers i am not saying that 
-            int l = find(i,partition,k,prefix_sum,dp);
-            // right part is returning the min sum of 'k' consecutive numbers
-            int r = find(partition+1,j,k,prefix_sum,dp);
-            // and then adding the both left and right to make it a single value and then goes to next patition and storing the mini for each partition and then finally returning
-            
-            ans = min(ans,l+r);
-        }
-        
-        if((j-i)%(k-1)==0)
-        {
-            ans+=prefix_sum[j+1]-prefix_sum[i];
-        }
-        
-        
-        return dp[i][j] = ans;
-    }
+    // Bottom-up (Tabulation)
 
     int mergeStones(vector<int>& stones, int k) {
         
-        int n = stones.size();
+         int n = stones.size();
         
         // agar k consecutive ka sum possible hi nhi hua tb
         if((n-1)%(k-1)!=0)
@@ -46,8 +18,44 @@ public:
             prefix_sum[i+1] = prefix_sum[i]+stones[i];
         }
         
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int i=0;
-        return find(i,n-1,k,prefix_sum,dp);
+        // Memoization
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        // logic
+        // in T.M: i=0 to i=j tk max and j=n-1
+        //         j=n-1 to j=i tk max
+        
+        // in B.T: i=n-1 to i=0 tk max
+        //         j=i+1 to j=n-1 tk max
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i+1;j<=n-1;j++)
+            {
+                int ans = INT_MAX;
+                
+                for(int partition=i;partition<j;partition+=(k-1))
+                {
+
+                    int l = dp[i][partition]; //find(i,partition,k,prefix_sum,dp);
+                    
+                    int r = dp[partition+1][j]; // find(partition+1,j,k,prefix_sum,dp);
+                    
+                    ans = min(ans,l+r);
+                }
+                
+                if((j-i)%(k-1)==0)
+                {
+                    ans+=prefix_sum[j+1]-prefix_sum[i];
+                }
+                
+                
+                dp[i][j] = ans;
+            }
+        }
+        
+        // int i=0;
+        // return find(i,n-1,k,prefix_sum,dp);
+        return dp[0][n-1];  // loop me jha pr i and j end ho rhe hai usi ko dekhkr final dp me se row,col return krna hai
     }
 };
