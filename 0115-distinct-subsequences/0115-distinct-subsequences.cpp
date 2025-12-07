dd7e793a48bfbc3e6529163674b1f3bcc8035481
TanishkChauhan09 <iamtanishkchauhan@gmail.com>
2025-12-07 16:13:50 +0530

    Time: 31 ms (49.99%), Space: 44.3 MB (31.67%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-
-    // back se recursion se solve krne pr and n-1,m-1 pass krne pr jiss se ki base case me 
-    // less than , greater than ka sign aa gya hai so hmm isko dp me initialize nhi krskte hai toh 
-    // meujhe base condition ko equals to ke sign me laana pdega so that fir hmm isko dp me initialize krke tabulation se krskte hai 
-
-    int find(int i,int j,string &str,string &t,vector<vector<int>>&dp)
-    {
-        // Base case
-        if(j<0)
-        return 1;
-        if(i<0)
-        return 0;
+    
+    // from last on passing n,m as for i,j
+    
+    // int find(int i,int j,string &str,string &t,vector<vector<int>>&dp)
+    // {
+    //     // Base case
+    //     if(j==0)
+    //     return 1;
+    //     if(i==0)
+    //     return 0;
         
-        if(dp[i][j]!=-1)
-        return dp[i][j];
+    //     if(dp[i][j]!=-1)
+    //     return dp[i][j];
 
-        // logic
-        if(str[i]==t[j])
-        return dp[i][j] = find(i-1,j-1,str,t,dp)+find(i-1,j,str,t,dp);
-        else
-        return dp[i][j] = find(i-1,j,str,t,dp);
-    }
+    //     // logic
+    //     if(str[i-1]==t[j-1])
+    //     return dp[i][j] = find(i-1,j-1,str,t,dp)+find(i-1,j,str,t,dp);
+    //     else
+    //     return dp[i][j] = find(i-1,j,str,t,dp);
+    // }
 
     int numDistinct(string s, string t) {
         
         int n = s.size();
         int m =t.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1)); 
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0)); 
 
-        return find(n-1,m-1,s,t,dp);
+        // initialization
+        for(int i=0;i<=n;i++)
+        dp[i][0] = 1;
+
+        // logic
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                dp[i][j] = dp[i-1][j-1]+dp[i-1][j]; // find(i-1,j-1,str,t,dp)+find(i-1,j,str,t,dp);
+                else
+                dp[i][j] = dp[i-1][j]; // find(i-1,j,str,t,dp);
+            }
+        }
+
+        return (int)dp[n][m]; // find(n,m,s,t,dp);
     }
 };
