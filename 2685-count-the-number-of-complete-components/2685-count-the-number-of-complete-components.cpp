8ae07faf37ee83cc79d40ff69037b43e4c271983
TanishkChauhan09 <iamtanishkchauhan@gmail.com>
2026-09-22 17:35:56 +0530

    Time: 11 ms (95.47%), Space: 117.7 MB (95.96%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,62 +1,84 @@
 class Solution {
 public:
+    
+    // By using DSU (Union by size kyuki isme hme ek component me kitne vertices hai wo chahiye jo sirf size se union krne pr possible hoskta hai)
+
 
     // if there are n vertices in a graph then there must be nC2 edges which is n(n-1)/2 .
 
-    void dfs(int node,vector<vector<int>>&adj,vector<int>&visited,int &v,int &e)
+    int findparent(int u,vector<int>&parent)
     {
-        visited[node] = 1;
-        v++;
-        e += adj[node].size();
+        if(u==parent[u])
+        return u;
 
-        // traversing on the neighbors
-        for(int j=0;j<adj[node].size();j++)
+        return parent[u] = findparent(parent[u],parent);
+    }
+
+    void unionbysize(int u,int v,vector<int>&parent,vector<int>&size)
+    {
+        int Pu = findparent(u,parent);
+        int Pv = findparent(v,parent);
+
+        if(Pu==Pv)
+        return;
+
+        if(size[Pu]>size[Pv])
         {
-            int neigh = adj[node][j];
-
-            if(!visited[neigh])
-            {
-                dfs(neigh,adj,visited,v,e);
-            }
+            parent[Pv] = Pu;
+            size[Pu] += size[Pv];
+        }
+        else 
+        {
+            parent[Pu] = Pv;
+            size[Pv] += size[Pu];
         }
     }
 
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
 
-        // making the adjacency list
-        vector<vector<int>>adj(n);
+        vector<int>parent(n,0);
+        vector<int>size(n,0);
+        
+        for(int i=0;i<n;i++) // initially all will be seperate components
+        {
+            parent[i] =i;
+            size[i] = 1;   // size ko initially 1 lena hoga sbhi ke liye adge 0 lenge toh 0+0 kuch nhi aayega bss 0 hi rhta isiliye taaki sum increase hopaaye unionbysize function me
+        }
 
         for(int i=0;i<edges.size();i++)
         {
             int u = edges[i][0];
             int v = edges[i][1];
 
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            unionbysize(u,v,parent,size);
         }
+ 
+       // now count the number of edges for each component
+       unordered_map<int,int>mp;
+       for(int i=0;i<edges.size();i++)
+       { 
+           int u = edges[i][0];
 
-        int ans = 0;
+           int prnt = findparent(u,parent);  // hrr wo edge jo ek hi component ka part hongi same hi parent me jaakr add kregi jiss se hme pta chal jayegi ke jiss poore component ka Ultimate parent hai usme kitni edge hone waali hai
+           mp[prnt]++;  
+       }
 
-        // graph will be in components 
-        vector<int>visited(n,0);
+       // finding the ans
+       int ans=0;
 
-        for(int i=0;i<n;i++)    // O( n*(V+E) )
-        {
-            if(!visited[i])
-            {
-                int v = 0;
-                int e = 0;
+       for(int i=0;i<n;i++)
+       {
+           if( findparent(i,parent) == i )
+           { 
+               int vertices = size[i];  // uss component ki vertices size array me hongi
+               int edges = mp[i];  // edges hmne phle hi uss parent ke respective, map me count kr rkhi hai
 
-                dfs(i,adj,visited,v,e);
+               if( ( vertices*(vertices-1)/2 ) ==  edges )
+                 ans++;
+           }
+       }
 
-                if( v*(v-1) == e)  // same edge do vertices ke liye add hojaayegi isiliye edges(e) me 2 se divide krta pr isme hmne dusre taraf ka 2 hta diya jiss se dono balance hogye   (v*(v-1)/2 == e/2) 
-                {
-                    ans++;
-                }
-            }
-        }
+       return ans;
 
-        return ans;
-        
-    }
+    }    
 };
