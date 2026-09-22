7fe1752e66453ab880aa7cfe2660c39ef0de13bc
TanishkChauhan09 <iamtanishkchauhan@gmail.com>
2026-09-22 15:43:51 +0530

    Time: 42 ms (87.06%), Space: 101.3 MB (88.43%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // yahan dhyaan de toh dependency dekh rhi hai ke jiss recipe ke sbhi ingredients milge phle wo bnegi anf uske baad wo bnenge jisme ye recipe indgredients ki tarah use ho rhi hogi toh dependency dikh thi hai isliye TOPO SORT(KAHN'S ALGORITHM) lga denge isme bnn main task adjacency list bnaane ka hai
+
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
 
         int n = recipes.size();
@@ -9,35 +12,53 @@ public:
         vector<int>visited(n,0);
         vector<string>result;
 
-        // Time comp( n*n*ingrediesnts[i].size() = n*n*m)
+        unordered_map<string,vector<int>> adj;
+        vector<int>indegree(n,0);
 
-        for(int i=0;i<n;i++)  // koi string kisi ki need ke baad bngyi toh fir unko doobara bnn ne ke liye mika dene ke liye ye loop chalaaya hai
+        // making the adjacency list
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<ingredients[i].size();j++) // recipe ke ingredients pr traverse krna
             {
-                if(visited[j]==1)
-                continue;
-
-                bool flag=1;
-
-                for(int k=0;k<ingredients[j].size();k++)
+                string ingredientOfRecipe = ingredients[i][j];
+                if(st.find(ingredientOfRecipe) == st.end()) // agr recipe ka ingredient supplier me hua hi nhi toh    ingredients---> recipe  edge bnaa denge  means recipe bnaane se phle ye ingredient chahiye hoga supplier me 
                 {
-                    if(st.find(ingredients[j][k]) == st.end())
-                    {
-                        flag=0;
-                        break;
-                    }
-                }
-
-                if(flag)
-                {
-                    visited[j] = 1;   // ye recipe bnn gyi hai donnara nhi bnaani ab isiliye iss index waali recipe ko visited mark kr diya hai
-                    result.push_back(recipes[j]);  // ye recipe bngyi to isse result me daal do
-                    st.insert(recipes[j]);  // ab ye recipe kisi aur recipe ke ingredients me aa skti hai toh isko me supplies me daal denge kyuki ab ye khud ready hai toh dusre kisi ka ingredients bnn skti hai
+                    adj[ingredientOfRecipe].push_back(i);
+                    indegree[i]++;     // u--->i edge
                 }
             }
         }
         
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty())
+        {
+            int idx = q.front();  // jo recipe kisi pe depent nhi hogi uski indegree me 0 hoga toh usi ka index bahar aayega
+            q.pop();
+
+            string recipe = recipes[idx];   // uss index pe konsi recipe hai wo nikaal lenge
+            result.push_back(recipe);       // uss recipe ko result me daal denge
+
+            for(auto it : adj[recipe])   // uss resipe pe jo jo index ki recipe dependent thi unki indegree ek se kmm krdenge and jo recipe ab kisi bhi ingredient pe depend nhi hogi usk iindegree 0 bnn chuki hogi and ussse bhi queue me daal denge ans me lene ke liye
+            {
+                int neigh = it;
+                indegree[neigh]--;
+
+                if(indegree[neigh]==0)
+                {
+                    q.push(neigh);
+                }
+            }
+        }
+
         return result;
+
     }
 };
