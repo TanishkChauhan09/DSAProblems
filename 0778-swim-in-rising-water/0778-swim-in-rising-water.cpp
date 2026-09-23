a543d1ad2ae8b377191c766e990ae3303a41531d
TanishkChauhan09 <iamtanishkchauhan@gmail.com>
2026-09-23 18:55:58 +0530

    Time: 1 ms (92.22%), Space: 14.8 MB (28.87%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,57 +1,67 @@
 class Solution {
 public:
+
+   // using binary search on answers(2D matrix)
+   // isme hmm ye dekhne ke jya uss decided time me (0,0) se (n-1,m-1) pr pahuch skte hai
+   
+    int drow[4] = {0,0,1,-1};
+    int dcol[4] = {1,-1,0,0};
+    
+    int isPossible(int i,int j,int n,int m,vector<vector<int>>&grid,vector<vector<int>>&visited,int mid)
+    {
+
+        if(visited[i][j] || grid[i][j]>mid)
+        {
+            return 0;
+        }
+
+        if(i==n-1 && j==m-1)
+            return 1;
+
+        visited[i][j] = 1;
+
+        for(int k=0;k<4;k++)
+        {
+            int n_row = i + drow[k];
+            int n_col = j + dcol[k];
+
+            if( n_row<0 || n_row>=n || n_col<0 || n_col>=m)
+            continue;
+
+            if(isPossible(n_row,n_col,n,m,grid,visited,mid))
+            return 1;
+            
+        }
+
+        return 0;
+    }
+
+    
     int swimInWater(vector<vector<int>>& grid) {
-
-
-        int drow[4] = {0,0,1,-1};
-        int dcol[4] = {1,-1,0,0};
 
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>>result(n,vector<int>(m,INT_MAX));
-        vector<vector<int>>explored(n,vector<int>(m,0));
-        result[0][0] = 0;
+        int start=0 , end = n*n , ans=0;
 
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq; 
-
-        pq.push({grid[0][0],{0,0}});  // 0 ki jagah grid[0][0] daala hai priority queue me
-
-        while(!pq.empty())
+        while(start<=end)
         {
-            int time = pq.top().first;
-            int row = pq.top().second.first;
-            int col = pq.top().second.second;
-
-            pq.pop();
-
-            if(row==n-1 && col==m-1)
-            return result[n-1][m-1];
-
-            if(explored[row][col])
-            continue;
-
-            explored[row][col] = 1;
-
-            for(int k=0;k<4;k++)
+            int mid = start+(end-start)/2;
+            
+            vector<vector<int>>visited(n,vector<int>(m,0));
+            if(isPossible(0,0,n,m,grid,visited,mid))
             {
-                int n_row = row + drow[k];
-                int n_col = col + dcol[k];
-
-                if(n_row<0 || n_row>=n || n_col<0 || n_col>=m || explored[n_row][n_col])
-                continue;
-
-                 int maxAnsPoss = max(time, grid[n_row][n_col]) ;
-
-                if( result[n_row][n_col] > maxAnsPoss )
-                {
-                    result[n_row][n_col] = maxAnsPoss;
-                    pq.push({maxAnsPoss , { n_row,n_col }});
-                }
+                ans = mid;
+                end = mid-1;
             }
+            else
+            {
+                start = mid+1;
+            }
+            
         }
-        
-        return result[n-1][m-1];
+       
+        return ans;
         
     }
 };
